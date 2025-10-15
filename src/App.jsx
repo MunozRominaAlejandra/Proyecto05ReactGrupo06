bd17297536ed40a1560de344cc16a258040890a2
MunozRominaAlejandra <romina.munoz.101516@gmail.com>
2025-10-14 22:38:08 -0300

    Inicio del proyecto 05

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Container} from 'react-bootstrap/Container';
+import AboutUs from './assets/Pages/AboutUs';
+import {Routes, Route} from 'react-router-dom';
+import Home from './assets/Pages/Home';
+import Game from './assets/Pages/Games';
+import Error from './assets/Pages/Error';
+import Layout from './assets/Components/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return(
+    <div>
+    </div>
+  )
+}
