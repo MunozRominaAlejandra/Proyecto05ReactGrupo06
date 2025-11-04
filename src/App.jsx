9f514fcae8c0cc01504c2f825e51bdf916481f8e
MunozRominaAlejandra <romina.munoz.101516@gmail.com>
2025-11-04 00:37:43 -0300

    Juego de estrella más Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './assets/Pages/Layout';
@@ -6,20 +5,19 @@ import Home from './assets/Pages/Home';
 import Game from './assets/Pages/Games';
 import AboutUs from './assets/Pages/AboutUs';
 import Error from './assets/Pages/Error';
+import './App.css';
+import JuegoEstrella from './assets/Components/JuegoEstrella';
 
-function App() {
+export default function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='games' element={<Game />} />
-          <Route path='aboutus' element={<AboutUs />} />
-          <Route path='*' element={<Error />} />
-        </Route>
-      </Routes>
-    </Container>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Game />} />
+        <Route path="/juegoestrella" element={<JuegoEstrella />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
   );
 }
-
-export default App;
